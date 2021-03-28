9ef7f1e829f431f2416b6c04faab7722535397d8
bestyxie <bestyxiejiayun@gmail.com>
2021-03-28 23:01:10 +0800

    feat: 交换组件

--- src/js/components/Brick/index.tsx
+++ src/js/components/Brick/index.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useMemo, useState, useCallback } from 'react'
 import { DndProvider, useDrag, useDrop, DragSourceMonitor, DropTargetMonitor } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import bricks from './mock'
 import { bricksItemShape } from './types'
+import { switchItem } from './util'
 
 const TYPE = 'card'
 
@@ -16,9 +17,13 @@ interface BrickWrapperProps {
   component: bricksItemShape
   parent: bricksItemShape[]
   index: number
+  moveItem: (dragParent: bricksItemShape[],
+    dragIndex: number,
+    hoverParent: bricksItemShape[],
+    hoverIndex?: number) => void
 }
 
-const BrickWrapper: React.FC<BrickWrapperProps> = ({ component, parent, index }): React.ReactElement => {
+const BrickWrapper: React.FC<BrickWrapperProps> = ({ component, parent, index, moveItem }): React.ReactElement => {
   const ref = useRef<HTMLDivElement>(null)
   const [{ isDragging }, drag] = useDrag({
     type: TYPE,
@@ -32,34 +37,50 @@ const BrickWrapper: React.FC<BrickWrapperProps> = ({ component, parent, index })
     }),
   }, [component, parent, index])
 
-  const [{ handleId }, drop] = useDrop({
+  const [{ handleId, isOverCurrent }, drop] = useDrop({
     accept: TYPE,
     collect: (monitor: DropTargetMonitor) => ({
       handleId: monitor.getHandlerId(),
+      isOverCurrent: monitor.isOver({ shallow: true })
     }),
     hover: (item: Item, monitor: DropTargetMonitor) => {
-      const {index: dragIndex, parent: dragParent} = item
-      const {index: hoverIndex, parent: hoverParent} = monitor.getItem<Item>()
-      if (dragParent !== hoverParent) {
-        const dragItem = dragParent.splice(dragIndex, 1)[0]
-        hoverParent.push(dragItem)
+      if (!monitor.isOver({ shallow: true })) return
+      const { index: dragIndex, parent: dragParent } = item
+      if (dragParent === parent) {
+        moveItem(dragParent, dragIndex, parent, index)
+        monitor.getItem<Item>().index = index
+        return
       }
-      console.log('dragParent', dragParent)
-      console.log('hoverParent', hoverParent)
+      moveItem(dragParent, dragIndex, parent)
+
+      const draggingItem = monitor.getItem<Item>()
+      draggingItem.index = parent.length
+      draggingItem.parent = parent
     }
-  })
+  }, [parent, moveItem])
 
   useEffect(() => {
     drag(drop(ref))
   }, [])
 
+  const style = useMemo(() => ({
+    opacity: isDragging ? 0 : 1,
+    background: isOverCurrent ? 'green' : 'white'
+  }), [isDragging, isOverCurrent])
+
   return (
-    <div ref={ref} style={{ opacity: isDragging ? 0 : 1 }} data-handler-id={handleId}>
+    <div ref={ref} style={style} data-handler-id={handleId}>
       {
         component.component.render({
           children: component.children && component.children.map((child, index) => {
             return (
-              <BrickWrapper component={child} parent={component.children} index={index} key={index} />
+              <BrickWrapper
+                component={child}
+                parent={component.children}
+                index={index}
+                key={index}
+                moveItem={moveItem}
+              />
             )
           })
         })
@@ -69,11 +90,29 @@ const BrickWrapper: React.FC<BrickWrapperProps> = ({ component, parent, index })
 }
 
 export default function Brick() {
+  const [brickList, setBrickList] = useState(bricks)
+  const moveItem = useCallback((dragParent, dragIndex, hoverParent, hoverIndex) => {
+    if (!dragParent.length) return
+    if (dragParent === hoverParent) {
+
+    } else {
+      const dragItem = dragParent.splice(dragIndex, 1)[0]
+      hoverParent.push(dragItem)
+    }
+    console.log(brickList)
+    setBrickList([...brickList])
+  }, [brickList, setBrickList])
   return (
     <div>
       <DndProvider backend={HTML5Backend}>
-        {bricks.map((b, index) => (
-          <BrickWrapper component={b} parent={bricks} index={index} key={index} />
+        {brickList.map((b, index) => (
+          <BrickWrapper
+            component={b}
+            parent={brickList}
+            index={index}
+            key={index}
+            moveItem={moveItem}
+          />
         ))}
       </DndProvider>
     </div>
---
+++ src/js/components/Brick/util.ts
@@ -0,0 +1,14 @@
+export const switchItem = <T extends Array<any>>(arr: T, from: number, to: number): T => {
+  let targetItem = arr[from]
+
+  if (to > from) {
+    let hoverItem = arr.splice(to, 1, targetItem)[0]
+    arr.splice(from, 1)
+    arr.splice(to - 1, 0, hoverItem)
+  } else {
+    let hoverItem = arr.splice(to, 1, targetItem)[0]
+    arr.splice(from, 1)
+    arr.splice(from + 1, 0, hoverItem)
+  }
+  return arr
+}
